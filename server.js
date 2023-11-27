dccb07ef454426046540a8ef1520171a262d7fca
MoSalah001 <mhmdsalah707@gmail.com>
2023-11-28 01:46:46 +0200

    change bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const User = require('./Models/User')
 const path = require("path")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 const app = express()
 require('dotenv').config()
