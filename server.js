56236151ab2417f78bb95e7842d775755418703e
MoSalah001 <mhmdsalah707@gmail.com>
2023-11-28 19:35:30 +0200

    deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 const app = express()
 require('dotenv').config()
-const port = 4000
+const port = 3010
 
 const admin = require('./Routes/admin')
 
