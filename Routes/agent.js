b25e52811dca481a25a74cd9d4530fdeedeb1248
MoSalah001 <mhmdsalah707@gmail.com>
2024-05-05 17:09:19 +0300

    fix path

--- Routes/agent.js
+++ Routes/agent.js
@@ -19,8 +19,7 @@ router.get('/quizez',async(req,res)=>{
 })
 
 router.get('/quizlist',async(req,res)=>{
-    res.send(path.join(__dirname,'../Client/branch','./quizList.html'))
-    // res.sendFile('./quizList.html',{root: path.join(__dirname,'../Client/branch')}) 
+    res.sendFile('./quizList.html',{root: path.join(__dirname,'../Client/agent')}) 
 })
 
 router.get('/getquizes',async (req,res)=>{
