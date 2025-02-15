e885a9e72b47dd69cf64c86e20c02b7a7dfba1eb
MoSalah001 <mhmdsalah707@gmail.com>
2025-02-15 14:41:02 +0200

    indexed DB

--- Client/js/reports.js
+++ Client/js/reports.js
@@ -19,7 +19,11 @@ window.onload = async()=>{
         objStore.createIndex('AgentName','AgentName',{unique: false})
         for(let i in data) {
             objStore.add(data[i])
-            console.log(data[i]);
+            console.log(data[i])
+        }
+        db.transaction("Quizes").objectStore("AgentName").get("V23MSAYED2").onsuccess = (event)=>{
+            console.log(`Result: ${event.target.result.name}`);
+            
         }
     }
 
